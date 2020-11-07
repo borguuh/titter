5605c50edf4dff928267b4b530a56dbdc039507b
Borguuh <abdrahmanadewale@gmail.com>
2020-11-07 01:01:12 +0100

    prepForDeployment

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3003;
-const middleware = require('./middleware')
-const path = require('path')
-const bodyParser = require("body-parser")
-const mongoose = require("./database");
+const middleware = require("./middleware");
+const path = require("path");
+const bodyParser = require("body-parser");
+// const mongoose = require("./database");
+const mongoose = require("mongoose");
 const session = require("express-session");
+require("dotenv").config();
 
-const server = app.listen(port, () => console.log("Server listening on port " + port));
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("**DB CONNECTED**"))
+  .catch((err) => console.log("DB CONNECTION ERR => ", err));
+
+const port = process.env.PORT || 3003;
+const server = app.listen(port, () =>
+  console.log("Server listening on port " + port)
+);
 const io = require("socket.io")(server, { pingTimeout: 60000 });
 
 app.set("view engine", "pug");
@@ -16,29 +31,31 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
+app.use(
+  session({
     secret: "bbq chips",
     resave: true,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+);
 
 // Routes
-const loginRoute = require('./routes/loginRoutes');
-const registerRoute = require('./routes/registerRoutes');
-const logoutRoute = require('./routes/logout');
-const postRoute = require('./routes/postRoutes');
-const profileRoute = require('./routes/profileRoutes');
-const uploadRoute = require('./routes/uploadRoutes');
-const searchRoute = require('./routes/searchRoutes');
-const messagesRoute = require('./routes/messagesRoutes');
-const notificationsRoute = require('./routes/notificationRoutes');
+const loginRoute = require("./routes/loginRoutes");
+const registerRoute = require("./routes/registerRoutes");
+const logoutRoute = require("./routes/logout");
+const postRoute = require("./routes/postRoutes");
+const profileRoute = require("./routes/profileRoutes");
+const uploadRoute = require("./routes/uploadRoutes");
+const searchRoute = require("./routes/searchRoutes");
+const messagesRoute = require("./routes/messagesRoutes");
+const notificationsRoute = require("./routes/notificationRoutes");
 
 // Api routes
-const postsApiRoute = require('./routes/api/posts');
-const usersApiRoute = require('./routes/api/users');
-const chatsApiRoute = require('./routes/api/chats');
-const messagesApiRoute = require('./routes/api/messages');
-const notificationsApiRoute = require('./routes/api/notifications');
+const postsApiRoute = require("./routes/api/posts");
+const usersApiRoute = require("./routes/api/users");
+const chatsApiRoute = require("./routes/api/chats");
+const messagesApiRoute = require("./routes/api/messages");
+const notificationsApiRoute = require("./routes/api/notifications");
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
@@ -57,38 +74,36 @@ app.use("/api/messages", messagesApiRoute);
 app.use("/api/notifications", notificationsApiRoute);
 
 app.get("/", middleware.requireLogin, (req, res, next) => {
+  var payload = {
+    pageTitle: "Home",
+    userLoggedIn: req.session.user,
+    userLoggedInJs: JSON.stringify(req.session.user),
+  };
 
-    var payload = {
-        pageTitle: "Home",
-        userLoggedIn: req.session.user,
-        userLoggedInJs: JSON.stringify(req.session.user),
-    }
+  res.status(200).render("home", payload);
+});
 
-    res.status(200).render("home", payload);
-})
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
 
-io.on("connection", socket => {
+  socket.on("join room", (room) => socket.join(room));
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+  socket.on("notification received", (room) =>
+    socket.in(room).emit("notification received")
+  );
 
-    socket.on("setup", userData => {
-        socket.join(userData._id);
-        socket.emit("connected");
-    })
+  socket.on("new message", (newMessage) => {
+    var chat = newMessage.chat;
 
-    socket.on("join room", room => socket.join(room));
-    socket.on("typing", room => socket.in(room).emit("typing"));
-    socket.on("stop typing", room => socket.in(room).emit("stop typing"));
-    socket.on("notification received", room => socket.in(room).emit("notification received"));
+    if (!chat.users) return console.log("Chat.users not defined");
 
-    socket.on("new message", newMessage => {
-        var chat = newMessage.chat;
-
-        if(!chat.users) return console.log("Chat.users not defined");
-
-        chat.users.forEach(user => {
-            
-            if(user._id == newMessage.sender._id) return;
-            socket.in(user._id).emit("message received", newMessage);
-        })
+    chat.users.forEach((user) => {
+      if (user._id == newMessage.sender._id) return;
+      socket.in(user._id).emit("message received", newMessage);
     });
-
-})
+  });
+});
--- database.js
+++ database.js
@@ -1,30 +1,35 @@
-//The require(‘mongoose’) call above returns a Singleton object. 
-//It means that the first time you call require(‘mongoose’), it 
-//is creating an instance of the Mongoose class and returning it. 
-//On subsequent calls, it will return the same instance that was 
-//created and returned to you the first time because of how module 
+//The require(‘mongoose’) call above returns a Singleton object.
+//It means that the first time you call require(‘mongoose’), it
+//is creating an instance of the Mongoose class and returning it.
+//On subsequent calls, it will return the same instance that was
+//created and returned to you the first time because of how module
 //import/export works in ES6.
-const mongoose = require("mongoose");
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useUnifiedTopology', true);
 
-class Database {
+// const mongoose = require("mongoose");
 
-    constructor() {
-        this.connect();
-    }
+// class Database {
+//   constructor() {
+//     this.mongoURI = process.env.MONGO_URI;
+//     this.connect();
+//   }
 
-    connect() {
-        mongoose.connect("mongodb://localhost:27017/twitter")
-        .then(() => {
-            console.log("database connection successful");
-        })
-        .catch((err) => {
-            console.log("database connection error " + err);
-        })
-    }
-}
+//   connect() {
+//     mongoose
+//       .connect(this.mongoURI, {
+//         useNewUrlParser: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true,
+//         useCreateIndex: true,
+//       })
+//       .then(() => {
+//         // console.log(process.env.MONGO_URI);
+//         console.log("database connection successful");
+//       })
+//       .catch((err) => {
+//         // console.log(process.env.MONGO_URI);
+//         console.log("database connection error " + err);
+//       });
+//   }
+// }
 
-module.exports = new Database();
+// module.exports = new Database();
